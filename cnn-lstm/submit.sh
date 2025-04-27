298411f012e6bd34cdef217648c7aed453f65d70
AJ <122406169+aj-broderick@users.noreply.github.com>
2025-04-26 21:07:38 -0400

    Add files via upload

---
+++ cnn-lstm/submit.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:T4:1
+#SBATCH --time=24:00:00
+#SBATCH --mem=256GB
+#SBATCH --nodes=2
+#SBATCH --job-name=pytest
+
+# Set the path to the virtual environment
+# export VENV_PATH="/work/data_science/ODU_CAPSTONE_2025/sns_venv/"
+
+# Activate the virtual environment
+source /work/data_science/ODU_CAPSTONE_2025/sns_env/bin/activate
+
+# Run Python commands within the virtual environment
+
+# python ~/ODU_CAPSTONE_2025/sns2025/driver.py train \
+#     --epochs 5 --batch_size 8 --learning_rate 1e-4 --latent_dim 32 \
+#     --model_path ~/ODU_CAPSTONE_2025/vae_bilstm_model.weights.h5 \
+#     --tensorboard_logdir logs/fit
+
+# python ~/ODU_CAPSTONE_2025/sns_cnn_lstm/driver.py --train
+# python ~/ODU_CAPSTONE_2025/sns_cnn_lstm/driver.py --test
+
+# python ~/ODU_CAPSTONE_2025/cnn_lstm/driver.py --train
+python ~/ODU_CAPSTONE_2025/cnn_lstm/driver.py --test
